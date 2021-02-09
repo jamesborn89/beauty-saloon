256de693c8a6ceb27797bdc439880126066f1de9
jamesborn89 <jamesborn89@gmail.com>
2021-02-09 15:31:35 +0400

    added

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -28,18 +28,34 @@ const swiperPrev = document.getElementById('swiperPrev')
 const swiperNext = document.getElementById('swiperNext')
 
 swiperPrev.addEventListener('click', () => {
-  masterSwiper.slidePrev();
+    masterSwiper.slidePrev();
 });
 swiperNext.addEventListener('click', () => {
-  masterSwiper.slideNext();
+    masterSwiper.slideNext();
+});
+
+var atmosphereMinSwiper = new Swiper('.swiper-container.atmosphere__min-slider', {
+    slidesPerView: 4,
+    spaceBetween: 40,
+    slidesPerGroup: 4,
+    loopedSlides: 5,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    loop: true,
 });
 
 var atmosphereSwiper = new Swiper('.swiper-container.atmosphere__slider', {
     slidesPerView: 2,
     spaceBetween: 40,
+    loopedSlides: 5,
+    loop: true,
     navigation: {
         nextEl: '.atmosphere__next',
         prevEl: '.atmosphere__prew',
+    },
+    thumbs: {
+        swiper: atmosphereMinSwiper
     },
 });
 
@@ -47,8 +63,8 @@ const atmosphereSwiperPrev = document.getElementById('atmosphereSwiperPrev')
 const atmosphereSwiperNext = document.getElementById('atmosphereSwiperNext')
 
 atmosphereSwiperPrev.addEventListener('click', () => {
-  atmosphereSwiper.slidePrev();
+    atmosphereSwiper.slidePrev();
 })
 atmosphereSwiperNext.addEventListener('click', () => {
-  atmosphereSwiper.slideNext();
+    atmosphereSwiper.slideNext();
 })
