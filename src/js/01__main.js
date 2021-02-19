0f6ae05a0cea723513edd99e70f7db2cc39d4526
jamesborn89 <jamesborn89@gmail.com>
2021-02-19 15:38:45 +0400

    added

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -16,6 +16,28 @@ var swiper = new Swiper('.home__slider', {
     },
 });
 
+var homeSwiper = new Swiper('.services-home__container.swiper-container', {
+    effect: 'fade',
+    speed: 1000,
+    autoplay: {
+        delay: 5000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+var varietiesSwiper = new Swiper('.varieties__item-slider.swiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 50,
+    navigation: {
+        nextEl: '.atmosphere__next',
+        prevEl: '.atmosphere__prew',
+    },
+});
+
 if (window.matchMedia("(max-width: 1199px)").matches) {
     const slider = document.querySelectorAll('.services__slider');
     const wrepper = document.querySelectorAll('.services__wrepper');
@@ -44,8 +66,7 @@ var masterSwiper = new Swiper('.swiper-container.services__slider', {
 });
 
 
-if (document.querySelector('.swiper-container')) {
-
+if (document.querySelector('.master__slider')) {
     var masterSwiper = new Swiper('.swiper-container.master__slider', {
         slidesPerView: 1,
         spaceBetween: 20,
@@ -92,7 +113,9 @@ if (document.querySelector('.swiper-container')) {
     swiperNext.addEventListener('click', () => {
         masterSwiper.slideNext();
     });
+}
 
+if (document.querySelector('.atmosphere__min-slider')) {
     var atmosphereMinSwiper = new Swiper('.swiper-container.atmosphere__min-slider', {
         slidesPerView: 3,
         spaceBetween: 10,
@@ -193,3 +216,120 @@ document.addEventListener('click', function (e) {
         toggleMenu();
     }
 });
+
+class Accordion {
+    constructor(el) {
+      // Store the <details> element
+      this.el = el;
+      // Store the <summary> element
+      this.summary = el.querySelector('summary');
+      // Store the <div class="content"> element
+      this.content = el.querySelector('.content');
+  
+      // Store the animation object (so we can cancel it if needed)
+      this.animation = null;
+      // Store if the element is closing
+      this.isClosing = false;
+      // Store if the element is expanding
+      this.isExpanding = false;
+      // Detect user clicks on the summary element
+      this.summary.addEventListener('click', (e) => this.onClick(e));
+    }
+  
+    onClick(e) {
+      // Stop default behaviour from the browser
+      e.preventDefault();
+      // Add an overflow on the <details> to avoid content overflowing
+      this.el.style.overflow = 'hidden';
+      // Check if the element is being closed or is already closed
+      if (this.isClosing || !this.el.open) {
+        this.open();
+      // Check if the element is being openned or is already open
+      } else if (this.isExpanding || this.el.open) {
+        this.shrink();
+      }
+    }
+  
+    shrink() {
+      // Set the element as "being closed"
+      this.isClosing = true;
+      
+      // Store the current height of the element
+      const startHeight = `${this.el.offsetHeight}px`;
+      // Calculate the height of the summary
+      const endHeight = `${this.summary.offsetHeight}px`;
+      
+      // If there is already an animation running
+      if (this.animation) {
+        // Cancel the current animation
+        this.animation.cancel();
+      }
+      
+      // Start a WAAPI animation
+      this.animation = this.el.animate({
+        // Set the keyframes from the startHeight to endHeight
+        height: [startHeight, endHeight]
+      }, {
+        duration: 400,
+        easing: 'ease-out'
+      });
+      
+      // When the animation is complete, call onAnimationFinish()
+      this.animation.onfinish = () => this.onAnimationFinish(false);
+      // If the animation is cancelled, isClosing variable is set to false
+      this.animation.oncancel = () => this.isClosing = false;
+    }
+  
+    open() {
+      // Apply a fixed height on the element
+      this.el.style.height = `${this.el.offsetHeight}px`;
+      // Force the [open] attribute on the details element
+      this.el.open = true;
+      // Wait for the next frame to call the expand function
+      window.requestAnimationFrame(() => this.expand());
+    }
+  
+    expand() {
+      // Set the element as "being expanding"
+      this.isExpanding = true;
+      // Get the current fixed height of the element
+      const startHeight = `${this.el.offsetHeight}px`;
+      // Calculate the open height of the element (summary height + content height)
+      const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+      
+      // If there is already an animation running
+      if (this.animation) {
+        // Cancel the current animation
+        this.animation.cancel();
+      }
+      
+      // Start a WAAPI animation
+      this.animation = this.el.animate({
+        // Set the keyframes from the startHeight to endHeight
+        height: [startHeight, endHeight]
+      }, {
+        duration: 400,
+        easing: 'ease-out'
+      });
+      // When the animation is complete, call onAnimationFinish()
+      this.animation.onfinish = () => this.onAnimationFinish(true);
+      // If the animation is cancelled, isExpanding variable is set to false
+      this.animation.oncancel = () => this.isExpanding = false;
+    }
+  
+    onAnimationFinish(open) {
+      // Set the open attribute based on the parameter
+      this.el.open = open;
+      // Clear the stored animation
+      this.animation = null;
+      // Reset isClosing & isExpanding
+      this.isClosing = false;
+      this.isExpanding = false;
+      // Remove the overflow hidden and the fixed height
+      this.el.style.height = this.el.style.overflow = '';
+    }
+  }
+  
+  document.querySelectorAll('details').forEach((el) => {
+    new Accordion(el);
+  });
