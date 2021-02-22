733bf990c9616b5422b6d93aa949fc3ea718fcd4
jamesborn89 <jamesborn89@gmail.com>
2021-02-22 12:28:40 +0400

    added

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -4,7 +4,7 @@ for (let i = 0; i < linksCount; i++)
     if (doc.URL.startsWith(doc.links[i].href))
         doc.links[i].classList.add('active');
 
-var swiper = new Swiper('.home__slider', {
+var homeSwiper = new Swiper('.home__slider', {
     effect: 'fade',
     speed: 1000,
     autoplay: {
@@ -13,6 +13,42 @@ var swiper = new Swiper('.home__slider', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+    },
+});
+
+var blogSwiper = new Swiper('.blog__slider.swiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 30,
+    autoHeight: true,
+
+    navigation: {
+        nextEl: '.atmosphere__next',
+        prevEl: '.atmosphere__prew',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            spaceBetween: 70,
+        },
+        960: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+            spaceBetween: 70,
+        },
+        1200: {
+            slidesPerView: 3,
+            slidesPerGroup: 1,
+            spaceBetween: 70,
+        }
     },
 });
 
@@ -40,10 +76,10 @@ var varietiesSwiper = new Swiper('.varieties__item-slider.swiper-container', {
 
 if (window.matchMedia("(max-width: 1199px)").matches) {
     const slider = document.querySelectorAll('.services__slider');
-    const wrepper = document.querySelectorAll('.services__wrepper');
+    const wrapper = document.querySelectorAll('.services__wrapper');
     const item = document.querySelectorAll('.services__item');
     slider.forEach(item => item.classList.toggle('swiper-container'));
-    wrepper.forEach(item => item.classList.toggle('swiper-wrapper'));
+    wrapper.forEach(item => item.classList.toggle('swiper-wrapper'));
     item.forEach(item => item.classList.toggle('swiper-slide'));
 }
 
@@ -219,117 +255,74 @@ document.addEventListener('click', function (e) {
 
 class Accordion {
     constructor(el) {
-      // Store the <details> element
-      this.el = el;
-      // Store the <summary> element
-      this.summary = el.querySelector('summary');
-      // Store the <div class="content"> element
-      this.content = el.querySelector('.content');
-  
-      // Store the animation object (so we can cancel it if needed)
-      this.animation = null;
-      // Store if the element is closing
-      this.isClosing = false;
-      // Store if the element is expanding
-      this.isExpanding = false;
-      // Detect user clicks on the summary element
-      this.summary.addEventListener('click', (e) => this.onClick(e));
+        this.el = el;
+        this.summary = el.querySelector('summary');
+        this.content = el.querySelector('.content');
+        this.animation = null;
+        this.isClosing = false;
+        this.isExpanding = false;
+        this.summary.addEventListener('click', (e) => this.onClick(e));
     }
-  
     onClick(e) {
-      // Stop default behaviour from the browser
-      e.preventDefault();
-      // Add an overflow on the <details> to avoid content overflowing
-      this.el.style.overflow = 'hidden';
-      // Check if the element is being closed or is already closed
-      if (this.isClosing || !this.el.open) {
-        this.open();
-      // Check if the element is being openned or is already open
-      } else if (this.isExpanding || this.el.open) {
-        this.shrink();
-      }
+        e.preventDefault();
+        this.el.style.overflow = 'hidden';
+        if (this.isClosing || !this.el.open) {
+            this.open();
+        } else if (this.isExpanding || this.el.open) {
+            this.shrink();
+        }
     }
-  
     shrink() {
-      // Set the element as "being closed"
-      this.isClosing = true;
-      
-      // Store the current height of the element
-      const startHeight = `${this.el.offsetHeight}px`;
-      // Calculate the height of the summary
-      const endHeight = `${this.summary.offsetHeight}px`;
-      
-      // If there is already an animation running
-      if (this.animation) {
-        // Cancel the current animation
-        this.animation.cancel();
-      }
-      
-      // Start a WAAPI animation
-      this.animation = this.el.animate({
-        // Set the keyframes from the startHeight to endHeight
-        height: [startHeight, endHeight]
-      }, {
-        duration: 400,
-        easing: 'ease-out'
-      });
-      
-      // When the animation is complete, call onAnimationFinish()
-      this.animation.onfinish = () => this.onAnimationFinish(false);
-      // If the animation is cancelled, isClosing variable is set to false
-      this.animation.oncancel = () => this.isClosing = false;
+        this.isClosing = true;
+        const startHeight = `${this.el.offsetHeight}px`;
+        const endHeight = `${this.summary.offsetHeight}px`;
+        if (this.animation) {
+            this.animation.cancel();
+        }
+        this.animation = this.el.animate({
+            height: [startHeight, endHeight]
+        }, {
+            duration: 400,
+            easing: 'ease-out'
+        });
+        this.animation.onfinish = () => this.onAnimationFinish(false);
+        this.animation.oncancel = () => this.isClosing = false;
     }
-  
+
     open() {
-      // Apply a fixed height on the element
-      this.el.style.height = `${this.el.offsetHeight}px`;
-      // Force the [open] attribute on the details element
-      this.el.open = true;
-      // Wait for the next frame to call the expand function
-      window.requestAnimationFrame(() => this.expand());
+        this.el.style.height = `${this.el.offsetHeight}px`;
+        this.el.open = true;
+        window.requestAnimationFrame(() => this.expand());
     }
-  
+
     expand() {
-      // Set the element as "being expanding"
-      this.isExpanding = true;
-      // Get the current fixed height of the element
-      const startHeight = `${this.el.offsetHeight}px`;
-      // Calculate the open height of the element (summary height + content height)
-      const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
-      
-      // If there is already an animation running
-      if (this.animation) {
-        // Cancel the current animation
-        this.animation.cancel();
-      }
-      
-      // Start a WAAPI animation
-      this.animation = this.el.animate({
-        // Set the keyframes from the startHeight to endHeight
-        height: [startHeight, endHeight]
-      }, {
-        duration: 400,
-        easing: 'ease-out'
-      });
-      // When the animation is complete, call onAnimationFinish()
-      this.animation.onfinish = () => this.onAnimationFinish(true);
-      // If the animation is cancelled, isExpanding variable is set to false
-      this.animation.oncancel = () => this.isExpanding = false;
+        this.isExpanding = true;
+        const startHeight = `${this.el.offsetHeight}px`;
+        const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+
+        if (this.animation) {
+            this.animation.cancel();
+        }
+
+        this.animation = this.el.animate({
+            height: [startHeight, endHeight]
+        }, {
+            duration: 400,
+            easing: 'ease-out'
+        });
+        this.animation.onfinish = () => this.onAnimationFinish(true);
+        this.animation.oncancel = () => this.isExpanding = false;
     }
-  
+
     onAnimationFinish(open) {
-      // Set the open attribute based on the parameter
-      this.el.open = open;
-      // Clear the stored animation
-      this.animation = null;
-      // Reset isClosing & isExpanding
-      this.isClosing = false;
-      this.isExpanding = false;
-      // Remove the overflow hidden and the fixed height
-      this.el.style.height = this.el.style.overflow = '';
+        this.el.open = open;
+        this.animation = null;
+        this.isClosing = false;
+        this.isExpanding = false;
+        this.el.style.height = this.el.style.overflow = '';
     }
-  }
-  
-  document.querySelectorAll('details').forEach((el) => {
+}
+
+document.querySelectorAll('details').forEach((el) => {
     new Accordion(el);
-  });
+});
