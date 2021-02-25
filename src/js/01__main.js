986488c8cca32781ce74f266b9e1a2ab755c8d48
jamesborn89 <jamesborn89@gmail.com>
2021-02-25 12:48:16 +0400

    added

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -332,3 +332,20 @@ class Accordion {
 document.querySelectorAll('details').forEach((el) => {
     new Accordion(el);
 });
+
+var modals = document.querySelectorAll('[data-modal]');
+
+modals.forEach(function(trigger) {
+  trigger.addEventListener('click', function(event) {
+    event.preventDefault();
+    var modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add('open');
+    var exits = modal.querySelectorAll('.modal-exit');
+    exits.forEach(function(exit) {
+      exit.addEventListener('click', function(event) {
+        event.preventDefault();
+        modal.classList.remove('open');
+      });
+    });
+  });
+});
